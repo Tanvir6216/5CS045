1acf03802f7e19bba93a1fa1ea8229bce142eca3
Shahrier Ahammed Tanvir <s.a.tanvir@wlv.ac.uk>
2025-10-30 15:44:42 +0000

    5cs045-task1-2442655.php

--- 5cs045-task1-2442655.php
+++ 5cs045-task1-2442655.php
@@ -1,68 +1,93 @@
 <!doctype html>
 <html lang="en">
-<body>
-<h1>List of ALL my movies!!!</h1>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>List of ALL my movies!!!</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="p-4">
+
+<h1>List of All My Movies!!!</h1>
+
 <?php
 // Connect to database
 include("db.php");
 
 // Run SQL query
-$sql = "SELECT * FROM movies ORDER BY date_of_release";
+$sql = "SELECT * FROM movies ORDER BY Date_of_release";
 $results = mysqli_query($mysqli, $sql);
+
+// Check if results exist
+if (!$results) {
+    die("Database query failed: " . mysqli_error($mysqli));
+}
 ?>
 
 <table class="table table-striped">
-	<?php while($a_row = mysqli_fetch_assoc($results)):?>
-		<tr>
-          <td><a href="movie-details.php?id=<?=$a_row['movie_id']?>"><?=$a_row['movie_name']?></a></td>
-          <td><?=$a_row['rating']?></td>
-		  <td><a class="btn btn-outline-danger btn-sm" href="delete-movie.php?id=<?=$a_row['movie_id']?>" role="button">Delete</a></td>
-		</tr>
-	<?php endwhile;?>
+  <thead>
+    <tr>
+      <th>Movie Name</th>
+      <th>Genre</th>
+      <th>Price (£)</th>
+      <th>Date of Release</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while($a_row = mysqli_fetch_assoc($results)): ?>
+      <tr>
+        <td><a href="movie-details.php?id=<?= $a_row['Movie_id'] ?>"><?= htmlspecialchars($a_row['Movie_name']) ?></a></td>
+        <td><?= htmlspecialchars($a_row['Genre']) ?></td>
+        <td><?= number_format($a_row['Price'], 2) ?></td>
+        <td><?= htmlspecialchars($a_row['Date_of_release']) ?></td>
+        <td>
+          <a class="btn btn-outline-danger btn-sm" href="delete-movie.php?id=<?= $a_row['Movie_id'] ?>" role="button">Delete</a>
+        </td>
+      </tr>
+    <?php endwhile; ?>
+  </tbody>
 </table>
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7"/>
-  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
-  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
-</svg> Add movie</button>
+<!-- Button to open modal -->
+<button type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Add Movie
+</button>
 
+<!-- Modal for adding a movie -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">New movie</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">New Movie</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form action="add-movie.php" method="post">
           <div class="mb-3">
             <label for="MovieName" class="col-form-label">Movie name:</label>
-            <input type="text" class="form-control" id="MovieName" name="MovieName">
+            <input type="text" class="form-control" id="MovieName" name="MovieName" required>
           </div>
           <div class="mb-3">
-            <label for="MovieDescription" class="col-form-label">Movie description:</label>
-            <textarea class="form-control" id="MovieDescription" name="MovieDescription"></textarea>
+            <label for="Genre" class="col-form-label">Genre:</label>
+            <input type="text" class="form-control" id="Genre" name="Genre" maxlength="20" required>
           </div>
           <div class="mb-3">
-            <label for="DateReleased" class="col-form-label">Movie release date:</label>
-            <input type="date" class="form-control" id="DateReleased" name="DateReleased">
-          </div>	
-		  <div class="mb-3">
-            <label for="MovieRating" class="col-form-label">Movie rating:</label>
-            <input type="number" class="form-control" id="MovieRating" name="MovieRating">
+            <label for="Price" class="col-form-label">Price (£):</label>
+            <input type="number" step="0.01" class="form-control" id="Price" name="Price" required>
           </div>
-		  
-		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-		  <button type="submit" class="btn btn-primary">Add</button>		  
+          <div class="mb-3">
+            <label for="DateReleased" class="col-form-label">Date of release:</label>
+            <input type="date" class="form-control" id="DateReleased" name="DateReleased" required>
+          </div>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Add</button>
         </form>
-	  </div>
-      <div class="modal-footer">
-
       </div>
     </div>
   </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
