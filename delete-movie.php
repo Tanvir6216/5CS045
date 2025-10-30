d32e78b3255080b09b0b438f49928bd8a735b703
Shahrier Ahammed Tanvir <s.a.tanvir@wlv.ac.uk>
2025-10-30 15:49:14 +0000

    delete-movie.php

--- delete-movie.php
+++ delete-movie.php
@@ -1,20 +1,7 @@
 <?php
-  
-  // Read values from the URL
-  $movie_id = $_GET['id'];
-  
-  // Connect to database
-  include("db.php");
-  
-  // Define SQL query
-  $sql = "DELETE FROM movies WHERE movie_id = " . $movie_id;
-  
-  // Run SQL statement and report errors
-  if(!$mysqli -> query($sql)) {
-      echo("<h4>SQL error description: " . $mysqli -> error . "</h4>");
-  }
-  
-  // Redirect to list
-  header("location: movies.php");  
-  
+include("db.php");
+$id = $_GET['id'];
+$sql = "DELETE FROM movies WHERE Movie_id = $id";
+mysqli_query($mysqli, $sql);
+header("Location: 5cs045-task1-2442655.php");
 ?>
